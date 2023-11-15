5091381406f3bd5c9a06c8f0a22372e81961ac9b
yuka toshima <tb19321yj@outlook.com>
2023-11-15 12:48:51 +1100

    week3 initial commit

---
+++ week3.py
@@ -0,0 +1,22 @@
+# fruits = ["a", "b", "c"]
+# for x in fruits:
+#     print(x)
+#     if x == "b":
+#         break
+
+# for x in range (6):
+#     print(x)
+
+# for x in range (5):
+#     print("*")
+
+# for x in range (5):
+#     print("*", end=" ") 
+
+# num = 2
+# exponent = 3
+# product = 1
+# for x in range (exponent):
+#     product = product * num
+#     print(product, end=" ")
+
