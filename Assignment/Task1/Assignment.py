fc5fe2b5b32022e6dd99951f98f02ecb2308dca0
yuka toshima <tb19321yj@outlook.com>
2023-11-29 14:39:39 +1100

    finished task 1

--- Assignment/Task1/Assignment.py
+++ Assignment/Task1/Assignment.py
@@ -1,37 +1,18 @@
 import glob # to seach the files
-from datetime import date # to know how many days between starting day and end day
-import math
-
-print("Code Petrol Gas Diesel")
-
-
 
 # Search all text files that contain 3 capital letters of the alphabet (country code)
 for file in glob.glob('[A-Z][A-Z][A-Z].txt'):
-  # assign starting day in start_day variable
-  start_day = date(2020, 3, 21)
-  end_day = date(2020, 12, 31)
-  # Calculate the number of days between the start and end dates to know the number of weeks
-  # abs() function is to provide the absolute value of the number of days
-  number_of_days = abs(start_day-end_day).days
-  # list_len is the number of rows for the unit prices and the starting day is including in the first day, so add 1
-  # Floor division: "//" operator rounds down to the nearest whole number
-  list_len = number_of_days//7 + 1
-  # print(list_len)
   # get country code
   country_code = file[:3]
   # set total price start with 0
   petrol_sum = 0
   gas_sum = 0
   diesel_sum= 0
+
   # set sum list 
-  # petrol_sum_list = []
-  # gas_sum_list = []
-  # diesel_sum_list = []
-
-
-
-  result_list = []
+  petrol_sum_list = []
+  gas_sum_list = []
+  diesel_sum_list = []
 
   # open county text file
   with open(file, 'r') as country_text_file:
@@ -45,37 +26,29 @@ for file in glob.glob('[A-Z][A-Z][A-Z].txt'):
           diesel = float(numbers[2])
           # print("diesel", diesel)
 
-          # print(petrol)
+          # add the price in each lines and assign to the variable (example: petrol_sum = petrol_sum + petrol)
           petrol_sum += petrol
           gas_sum += gas
           diesel_sum += diesel
 
-          # petrol_sum_list.append(petrol)
-          # gas_sum_list.append(gas)
-          # diesel_sum_list.append(diesel)
+          petrol_sum_list.append(petrol)
+          gas_sum_list.append(gas)
+          diesel_sum_list.append(diesel)
 
-      # print("petrol sum:", petrol_sum)
-      # print("gas sum:", gas_sum)
-      # print("diesel sum:", diesel_sum)
+  # average of each unit prices
+      petrol_ave = petrol_sum/len(petrol_sum_list) 
+      gas_ave = gas_sum/len(gas_sum_list)
+      diesel_ave = diesel_sum/len(diesel_sum_list)
 
-      # print("list", petrol_sum_list)
-
-      # print(petrol_len)
-  petrol_ave = petrol_sum/list_len
-  # print("petrol ave",petrol_ave)
-    
-  gas_ave = gas_sum/list_len
-  # print("gas average", gas_ave)
-
-  diesel_ave = diesel_sum/list_len
-  # print("diesel average", diesel_ave)
   
 
-  # print("Code Petrol Gas Diesel")
-  # print(country_code,"{%5d} {%8d} {%12d}".format((petrol_ave, gas_ave, diesel_ave)))
-  # print("{} {:>5.2f} {:>5.2f} {:>5.2f}".format(country_code,petrol_ave, gas_ave, diesel_ave))
-  result_table = "{} {:>5.2f} {:>5.2f} {:>5.2f}".format(country_code,petrol_ave, gas_ave, diesel_ave)
+  result = "{:.2f} {:.2f} {:.2f}".format(petrol_ave, gas_ave, diesel_ave)
+  
+  # open file with write mode and name it COUNTRY_CODE_ave.txt
+  f = open(country_code + "_ave.txt", 'w')
+  # write the result in the file
+  f.write(result)
+  # close the file
+  f.close
 
-
-print(result_table)
-
+  
---
+++ week5.py
@@ -0,0 +1,54 @@
+# first = [1,2,3,4]
+# second = list(range(1,5))
+# print(first)
+# print(second)
+# # [1, 2, 3, 4]
+# # [1, 2, 3, 4]
+
+
+# third = first + [5,6]
+# print(third)
+# # [1, 2, 3, 4, 5, 6]
+# print(third[4]) #5
+
+
+# nums = [1, 2, 3, 4, 5, 6]
+# index = 0
+# while index < len(nums):
+#     nums[index] = nums[index] ** 2
+#     index += 1
+# print(nums)
+# # [1, 4, 9, 16, 25, 36]
+
+# ex = [1,2,3,4]
+# print(ex.index(3)) #2
+# ex.insert(1, 10)
+# print(ex) 
+# # [1, 10, 2, 3, 4]
+
+num = [2,5,1,9,4]
+sort_num = num.sort()
+print(sort_num)
+
+
+# ####------ Tuple
+# # tuple: can not change
+
+
+# ####------ Function
+
+# def main():
+#     number = float(input("Enter number:"))
+#     result = square(number)
+#     print("The square of", number, "is", result)
+# def square(x):
+#     return x * x
+# main()
+
+# ####------ Dictionary
+# # dictionary = {key, value}
+
+# person_info = {"name": "monky","phone": 123,"mail": "aaaa"} 
+# print(person_info["name"]) #monky
+# print(person_info["phone"]) #123
+
