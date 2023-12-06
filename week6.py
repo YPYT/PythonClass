ddf12bd506e39f7dafde8d5868e8a2545f6b35e6
yuka toshima <tb19321yj@outlook.com>
2023-12-06 12:27:08 +1100

    added more comment

--- week6.py
+++ week6.py
@@ -2,17 +2,20 @@
 ## Concept: 1. define function
 ##          2. call function
 
+#1. Function define
 def greeting(name):
     print("Hello, " + name)
-
+#2. Function call
 greeting("Emily")
 greeting("Jessie")
 greeting("Lisa")
 
+
+#1. Function define
 def addition(num1, num2):
     total = num1 + num2
     print("The total is:", total)
-
+# Function call
 addition(3,8)
 addition(2872, 1762583)
 addition(1.4, 2.4)
