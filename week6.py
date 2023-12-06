7e4e853d3b0a6dd25f87cc5bf1bfce9b41c23412
yuka toshima <tb19321yj@outlook.com>
2023-12-06 12:17:16 +1100

    added week6 python class file

---
+++ week6.py
@@ -0,0 +1,24 @@
+### --- Function (for reusability)
+def greeting(name):
+    print("Hello, " + name)
+
+greeting("Emily")
+greeting("Jessie")
+greeting("Lisa")
+
+def addition(num1, num2):
+    total = num1 + num2
+    print("The total is:", total)
+
+addition(3,8)
+addition(2872, 1762583)
+addition(1.4, 2.4)
+
+def my_function():
+    print("This is my function")
+def main():
+    my_function()
+main()
+
+
+    
